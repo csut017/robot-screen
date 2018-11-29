43a565c49715f1fe81fdabfb58b5bf187d9f06c3
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2018-11-29 13:05:51 +1300

    Implemented reconnecting from the main screen.

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -15,29 +15,65 @@ export class MainComponent implements OnInit {
     public websocket: WebsocketService) { }
 
   info: WebsocketStatus = new WebsocketStatus('Initialising...');
+  connecting: boolean;
   currentTime: string = 'Initialising';
   currentView: string = 'face';
+  firstTime: boolean = true;
+  timeToConnect: moment.Moment;
+  timeToConnectDisplay: string;
+  private lastConnectionTime: number = 5;
 
   ngOnInit() {
-    interval(1000).subscribe(_ => this.currentTime = moment().format("dddd, Do MMMM YYYY, h:mm a"));
+    interval(500).subscribe(_ => {
+      this.currentTime = moment().format("dddd, Do MMMM YYYY, h:mm a");
+      this.checkToConnect();
+    });
     const id = this.route.snapshot.paramMap.get('id');
-    let firstTime = true;
-    this.websocket.initialise()
-      .subscribe(info => {
-        this.info = info;
-        if (firstTime && info.connected) {
-          this.currentView = id || 'face';
-          if (!id) {
-            this.websocket.fetchCurrentView()
-              .subscribe(view => this.currentView = view || 'face');
-          }
-          firstTime = false;
-        };
-      });
+    this.connectToServer(id);
     this.websocket.viewChanged.subscribe(screen => {
       screen = screen || 'face';
       console.log(`[Main] Changing screen to ${screen}`);
       this.currentView = screen
     });
+  }
+
+  checkToConnect(): void {
+    if (this.connecting || this.info.connected) return;
+    const inSeconds = this.timeToConnect.diff(moment(), 'seconds');
+    if (inSeconds >= 60) {
+      this.timeToConnectDisplay = this.timeToConnect.fromNow();
+    } else if (inSeconds == 1) {
+      this.timeToConnectDisplay = `in 1 second`;
+    } else {
+      this.timeToConnectDisplay = `in ${inSeconds} seconds`;
+    }
+    if (this.timeToConnect.isSameOrBefore(moment())) this.connectToServer();
+  }
+
+  connectToServer(id?: string): void {
+    console.log(`[Main] Connecting to websocket`);
+    this.connecting = true;
+    this.websocket.initialise()
+      .subscribe(info => {
+        this.connecting = info.status == 'Connecting...';
+        this.info = info;
+        if (this.connecting) return;
+
+        if (this.firstTime && info.connected) {
+          this.currentView = id || 'face';
+          if (!id) {
+            this.websocket.fetchCurrentView()
+              .subscribe(view => this.currentView = view || 'face');
+          }
+          this.firstTime = false;
+        };
+        if (!info.connected) {
+          this.lastConnectionTime *= 2;
+          this.timeToConnect = moment().add(this.lastConnectionTime, 'seconds');
+          console.log(`[Main] Unable to connect, trying again in ${this.lastConnectionTime}s`);
+        } else {
+          this.lastConnectionTime = 5;
+        }
+      });
   }
 }
