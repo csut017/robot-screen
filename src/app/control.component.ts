7ab0a347ed0cc0ae0c80d14019a6b0ed802e620d
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2018-11-30 18:07:46 +1300

    Changed the debug message format.

--- src/app/control.component.ts
+++ src/app/control.component.ts
@@ -6,6 +6,11 @@ import { interval } from 'rxjs';
 class ViewDataItem {
   constructor(public name: string,
     public value: string) { }
+}
+
+class DebugLine {
+  constructor(public lineType: string,
+    public value: string) { }
 
   details: any;
 }
@@ -27,7 +32,7 @@ export class ControlComponent implements OnInit {
   selectedEvent: string;
   scheduledEvents: ScheduledEvent[] = [];
   viewData: ViewDataItem[] = [];
-  logData: ViewDataItem[] = [];
+  logData: DebugLine[] = [];
 
   ngOnInit() {
     interval(1000).subscribe(_ => this.currentTime = moment().format("dddd, Do MMMM YYYY, h:mm a"));
@@ -41,16 +46,23 @@ export class ControlComponent implements OnInit {
   }
 
   private addDebug(msg: DebugMessage): void {
-    let name = '<unknown>: ' + msg.type,
+    let name = 'unknown',
       value = '';
     switch (msg.type) {
       case 'FUN_CALL':
-        name = 'Function';
+        name = 'chat-bubble';
         value = msg.details['function'];
+        let args = [],
+          argList = msg.details['arguments'];
+        for (let key in argList) {
+          let argVal = argList[key];
+          args.push(`${key}='${argVal}'`);
+        }
+        value += '(' + args.join(',') + ')';
         break;
     }
 
-    let item = new ViewDataItem(name, value);
+    let item = new DebugLine(name, value);
     item.details = msg.details;
     this.logData.splice(0, 0, item);
   }
