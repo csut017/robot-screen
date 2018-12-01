c6f61990e5a20943471afd22d15a59d3df968850
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2018-12-01 14:22:13 +1300

    Moved the debug line generation into DebugLine.

--- src/app/debugger.component.ts
+++ src/app/debugger.component.ts
@@ -38,34 +38,6 @@ export class DebuggerComponent implements OnInit {
   }
 
   private addDebug(msg: DebugMessage): void {
-    let name = 'error-standard',
-      value = msg.type;
-    switch (msg.type) {
-      case 'FUN_CALL':
-        name = 'redo';
-        value = msg.details['function'];
-        let args = [],
-          argList = msg.details['arguments'];
-        for (let key in argList) {
-          let argVal = argList[key];
-          args.push(`${key}='${argVal}'`);
-        }
-        value += '(' + args.join(', ') + ')';
-        break;
-
-      case 'RES_LOOKUP':
-        name = 'search';
-        value = `RESOURCE[${msg.details.type}]: ${msg.details.resource}`;
-        break;
-
-        case 'SCRIPT_START':
-        name = 'play';
-        value = `SCRIPT: ${msg.details.name}`;
-        break;
-      }
-
-    let item = new DebugLine(name, value);
-    item.details = msg.details;
-    this.logData.splice(0, 0, item);
+    this.logData.splice(0, 0, DebugLine.FromMessage(msg));
   }
 }
