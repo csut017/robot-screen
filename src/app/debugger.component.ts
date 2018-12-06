091f38171fa2c3e87291e4c9444956aa0c299fa1
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2018-12-06 19:00:02 +1300

    Open a script if a line in it is selected.

--- src/app/debugger.component.ts
+++ src/app/debugger.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WebsocketStatus, WebsocketService, DebugMessage } from './websocket.service';
 import { DebugLine } from './debug-line';
-import { AbstractSyntaxTree } from './abstract-syntax-tree';
+import { AbstractSyntaxTree, ASTNode } from './abstract-syntax-tree';
 import { CallTable } from './call-table';
 
 @Component({
@@ -18,6 +18,7 @@ export class DebuggerComponent implements OnInit {
   ast: AbstractSyntaxTree;
   callTable: CallTable = new CallTable();
   currentNodeID: string;
+  private scriptMap: { [index: string]: AbstractSyntaxTree } = {};
 
   ngOnInit() {
     this.websocket.debugChanged.subscribe(msg => this.addDebug(msg));
@@ -37,12 +38,23 @@ export class DebuggerComponent implements OnInit {
   processItem(item: DebugLine): void {
     item.isOpen = !item.isOpen;
     this.currentNodeID = item.details.node_id;
-    if (item.details.ast) {
+    let ast = item.details.ast;
+    if (ast) {
       console.groupCollapsed('[Debugger] Opening script');
       const ast = new AbstractSyntaxTree(item.details.name, item.details.ast);
       console.log(ast);
       this.ast = ast;
       console.groupEnd();
+    }
+
+    if (!ast && this.currentNodeID) {
+      ast = this.scriptMap[this.currentNodeID];
+      if (ast) {
+        console.groupCollapsed('[Debugger] Opening script for line');
+        console.log(ast);
+        this.ast = ast;
+        console.groupEnd();
+      }
     }
   }
 
@@ -50,6 +62,7 @@ export class DebuggerComponent implements OnInit {
     this.websocket.fetchDebugLog()
       .subscribe(result => {
         let items = result || [];
+        this.scriptMap = {};
         items.forEach(item => this.addDebug(item));
       });
   }
@@ -57,5 +70,17 @@ export class DebuggerComponent implements OnInit {
   private addDebug(msg: DebugMessage): void {
     this.callTable.increment(msg.details.node_id);
     this.logData.splice(0, 0, DebugLine.FromMessage(msg));
+    if (msg.details.ast) {
+      const ast = new AbstractSyntaxTree(msg.details.name, msg.details.ast);
+      ast.nodes.forEach(child => {
+        this.mapNode(ast, child);
+      });
+    }
+  }
+
+  private mapNode(ast: AbstractSyntaxTree, node: ASTNode) {
+    this.scriptMap[node.id] = ast;
+    (node.children || []).forEach(n => this.mapNode(ast, n));
+    (node.args || []).forEach(n => this.mapNode(ast, n));
   }
 }
